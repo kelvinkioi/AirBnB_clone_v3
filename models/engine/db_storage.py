06b0b12ece19fcf51ab1706c8b7ebfa5e5b31018
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-10-29 01:55:40 -0700

    Update DBStorage and FileStorage, adding two new methods

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,22 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """
+        method to retrieve one object
+        """
+        if cls in classes.values() and id and type(id) == str:
+            db_obj = self.all(cls)
+            for key, value in db_obj.items():
+                if key.split(".")[1] == id:
+                    return value
+
+    def count(self, cls=None):
+        """
+        method to count the number of objects in storage
+        """
+        n_obj = self.all(cls)
+        if cls in classes.values():
+            n_obj = self.all(cls)
+        return len(n_obj)
