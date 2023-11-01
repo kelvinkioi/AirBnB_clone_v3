be62efa023937f83a8ed3df561f872294a5351a1
fahrenh8T <raymondrayn98@gmail.com>
2023-11-01 06:02:18 +0300

    /api && /models patch

--- models/base_model.py
+++ models/base_model.py
@@ -21,7 +21,7 @@ else:
 
 class BaseModel:
     """The BaseModel class from which future classes will be derived"""
-    if getenv("HBNB_TYPE_STORAGE") == "db":
+    if models.storage_t == "db":
         id = Column(String(60), primary_key=True)
         created_at = Column(DateTime, default=datetime.utcnow)
         updated_at = Column(DateTime, default=datetime.utcnow)
